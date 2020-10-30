b68a760e5190244982dfdf412c526fbb18b81017
Jörg Winkler <j.winkler@fu-berlin.de>
2020-10-30 09:47:19 +0100

    add a secondary structure class

--- include/motif_store.hpp
+++ include/motif_store.hpp
@@ -5,6 +5,35 @@
 namespace mars
 {
 
+//!\brief Store the type of statistics values.
+typedef uint32_t value_type;
+
+//!\brief Store {start, end} positions of an inverval.
+typedef std::pair<value_type, value_type> interval_type;
+
+//!\brief Store {min, median, max} of a distribution.
+typedef std::tuple<value_type, value_type, value_type> stat_type;
+
+class secondary_structure
+{
+public:
+    //enum loop_type {stem, hairpin, bulge, internal};
+    std::string loop_fwd;
+    std::string loop_bwd;
+    std::string stem;
+
+    stat_type length;
+    // TODO: gaps
+};
+
+class stem_loop_motif
+{
+private:
+    interval_type bounds;
+    stat_type length;
+    std::vector<secondary_structure> elements;
+};
+
 class motif_store
 {
 private:
